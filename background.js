6f0be436f5d8970b3a040fa008295c32f0c14637
Planck35 <chaoyuw@andrew.cmu.edu>
2019-04-03 23:41:58 -0700

    push

--- background.js
+++ background.js
@@ -1,13 +1,26 @@
-var whitelist;
+var popHTML = "chrome-extension://" + chrome.runtime.id + "/pop.html";
+var popJS = "chrome-extension://" + chrome.runtime.id + "/pop.js";
+
+console.log(popJS);
+var whitelist = {
+    popHTML: true,
+    popJS: true
+};
 
 chrome.storage.onChanged.addListener((changes, areaName) => {
+    console.log(changes);
     if (changes["whitelist"] != undefined) {
-        whitelist = changes["whitelist"];
+        whitelist = changes["whitelist"]["newValue"];
     }
 });
 
+chrome.storage.sync.get(["whitelist"], (result) => {
+    console.log(result.whitelist);
+});
 
 chrome.webRequest.onBeforeRequest.addListener((details) => {
+    console.log(details.url);
+    // console.log(chrome.runtime.id);
     var thisURL = new URL(details.url);
     var hostName = thisURL.host;
 
--- pop.js
+++ pop.js
@@ -1,39 +1,44 @@
-function loadHeader() {
-    chrome.storage.sync.get('headPolicies', (data) => {
-        if (data.headPolicies == undefined) {
-            document.getElementById("header").innerHTML = "None";
-        } else {
-            document.getElementById("header").innerHTML = data.headPolicies.replace(/\n/g, "<br />");;
-        }
-    });
+// function loadHeader() {
+//     chrome.storage.sync.get('headPolicies', (data) => {
+//         if (data.headPolicies == undefined) {
+//             document.getElementById("header").innerHTML = "None";
+//         } else {
+//             document.getElementById("header").innerHTML = data.headPolicies.replace(/\n/g, "<br />");;
+//         }
+//     });
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {
-                file: 'scriptHTML.js'
-            }, () => {
-                chrome.storage.sync.get('metaPolicies', (data) => {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id,
+//             {
+//                 file: 'scriptHTML.js'
+//             }, () => {
+//                 chrome.storage.sync.get('metaPolicies', (data) => {
 
-                    if (data.metaPolicies == undefined || data.metaPolicies == "") {
-                        document.getElementById("tags").innerHTML = "None";
-                    } else {
-                        document.getElementById("tags").innerHTML = data.metaPolicies.replace(/\n/g, "<br />");
-                    }
-                });
-                chrome.storage.sync.get('xFrame', (data) => {
-                    if (data.xFrame == undefined || data.xFrame == "") {
-                        document.getElementById("xFrame").innerHTML = "None";
-                    } else {
-                        document.getElementById("xFrame").innerHTML = data.xFrame;
-                    }
-                });
-            });
-    });
-}
+//                     if (data.metaPolicies == undefined || data.metaPolicies == "") {
+//                         document.getElementById("tags").innerHTML = "None";
+//                     } else {
+//                         document.getElementById("tags").innerHTML = data.metaPolicies.replace(/\n/g, "<br />");
+//                     }
+//                 });
+//                 chrome.storage.sync.get('xFrame', (data) => {
+//                     if (data.xFrame == undefined || data.xFrame == "") {
+//                         document.getElementById("xFrame").innerHTML = "None";
+//                     } else {
+//                         document.getElementById("xFrame").innerHTML = data.xFrame;
+//                     }
+//                 });
+//             });
+//     });
+// }
 
-function debug() {
+// function debug() {
 
-}
-debug();
-loadHeader();
+// }
+// debug();
+// loadHeader();
+
+chrome.storage.sync.set({ "whitelist": { "www.facebook.com": true } });
+chrome.storage.sync.get(["whitelist"], (result) => {
+    console.log(result.whitelist);
+});
