a1c0a1d5c26972d30365d343dcf36c649681c1ae
Planck35 <chaoyuw@andrew.cmu.edu>
2019-04-04 00:46:25 -0700

    bl wl

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 var whitelist = {};
-
+var blacklist = {};
 whitelist[chrome.runtime.id] = true;
 
 chrome.runtime.onInstalled.addListener((details) => {
     chrome.storage.sync.set({ "whitelist": whitelist });
+    chrome.storage.sync.set({ "blacklist": blacklist });
 });
 
 chrome.storage.sync.get(["whitelist"], (result) => {
@@ -11,6 +12,13 @@ chrome.storage.sync.get(["whitelist"], (result) => {
     console.log(result.whitelist);
     console.log("----");
     whitelist = result.whitelist;
+});
+
+chrome.storage.sync.get(["blacklist"], (result) => {
+    console.log("****");
+    console.log(result.blacklist);
+    console.log("****");
+    blacklist = result.blacklist;
 });
 
 
@@ -21,9 +29,13 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
 
     if (changes["whitelist"] != undefined) {
         chrome.storage.sync.get(["whitelist"], (result) => {
-
             whitelist = result.whitelist;
+        });
+    }
 
+    if (changes["blacklist"] != undefined) {
+        chrome.storage.sync.get(["blacklist"], (result) => {
+            blacklist = result.blacklist;
         });
     }
 });
@@ -37,12 +49,30 @@ chrome.webRequest.onBeforeRequest.addListener((details) => {
     var hostName = thisURL.host;
     // console.log(hostName);
 
-    if (whitelist[hostName] == undefined) {
+    if (whitelist[hostName] == undefined && blacklist[hostName] == undefined) {
         // console.log("blocking" + details.url);
+        chrome.storage.sync.get(["requests"], (result) => {
+            var requests;
+            if (result.requests == undefined) {
+                requests = {};
+            } else {
+                requests = result.requests;
+            }
+            if (requests[hostname] != true) {
+                requests[hostName] = true;
+                chrome.storage.sync.set({ "requests": requests });
+            }
+        });
         return { cancel: true };
-    } else {
+    } else if (whitelist[hostName] != undefined && blacklist[hostName] == undefined) {
         // console.log("good to go" + details.url);
         return { cancel: false };
+    } else if (blacklist[hostName] != undefined && whitelist[hostName] == undefined) {
+        // console.log("Not good to go" + details.url);
+        return { cancel: true };
+    } else {
+        console.log("error: appear on both WL and BL")
+        return { cancel: true };
     }
 },
     { urls: ["<all_urls>"] },
