d1eefb17054cafc4b0394334198d3cfe745e6d9b
Planck35 <chaoyuw@andrew.cmu.edu>
2019-04-03 23:07:46 -0700

    add event drive schema

--- background.js
+++ background.js
@@ -1,71 +1,22 @@
-var tabIndex;
-var windowIndex;
+var whitelist;
 
-var csp = {};
-var useHttps = false;
-
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == "complete") {
-
-        tabIndex = tab.index;
-        windowIndex = tab.windowId;
-        // alert(`tabIndex ${tabIndex} windowIndex${windowIndex}`);
-        var output = "";
-        if (useHttps) {
-            output = "Use https: TRUE\n";
-        } else {
-            output = "Use https: FALSE\n";
-        }
-        for (var key in csp) {
-            if (key == 'upgrade-insecure-requests') {
-                output = output + 'upgrade-insecure-requests' + '\n';
-            } else {
-                var temp = Array.from(csp[key]);
-                output = output + key.toString() + ':' + temp.join(',') + '\n';
-            }
-        }
-
-        chrome.storage.sync.set({ headPolicies: output }, () => {
-            console.log("finish storing head Policies");
-        });
-        // alert(output);
-        csp = {};
+chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (changes["whitelist"] != undefined) {
+        whitelist = changes["whitelist"];
     }
 });
 
 
-chrome.webRequest.onHeadersReceived.addListener((details) => {
-    if (details.url.slice(0, 5).toUpperCase() == "HTTPS") {
-        useHttps = true;
-    }
-    for (var pointer = 0; pointer < details.responseHeaders.length; pointer += 1) {
-        if (details.responseHeaders[pointer]["name"] == "content-security-policy") {
-            if (details.responseHeaders[pointer]["value"] == 'upgrade-insecure-requests') {
-                csp['upgrade-insecure-requests'] = 'upgrade-insecure-requests';
-            } else {
-                var rawInput = details.responseHeaders[pointer]["value"];
-                var splited = rawInput.split(';');
-                for (var index = 0; index < splited.length; index++) {
-                    var subPolicy = splited[index].trim();
-                    var subPolicySplit = subPolicy.split(' ');
-                    var subPolicyName = subPolicySplit[0];
-                    var subPolicyArray = subPolicySplit.slice(1);
-                    if (csp[subPolicyName] == undefined) {
-                        csp[subPolicyName] = new Set([]);
-                    }
-                    for (var subIndex = 0; subIndex < subPolicyArray.length; subIndex++) {
-                        csp[subPolicyName].add(subPolicyArray[subIndex]);
-                    }
-                }
-            }
-        } else if (details.responseHeaders[pointer]["name"] == "x-frame-options") {
-            chrome.storage.sync.set({ xFrame: details.responseHeaders[pointer]["value"] }, () => {
-                console.log("finish storing X-Frame Policies");
-            });
-        }
-    }
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+    var thisURL = new URL(details.url);
+    var hostName = thisURL.host;
 
+    if (whitelist[hostName] == undefined) {
+        return { cancel: true };
+    } else {
+        return { cancel: false };
+    }
 },
-    { urls: ['https://*/*', 'http://*/*'] },
-    ['responseHeaders']);
+    { urls: ["<all_urls>"] },
+    ["blocking", "requestBody"]
+);
