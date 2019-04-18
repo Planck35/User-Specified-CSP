1559afc59e92b91cb7fc673ed7216162653ed89f
ErguiXie <ergui.xie@sv.cmu.edu>
2019-04-17 23:20:56 -0700

    can add url to list

--- options.js
+++ options.js
@@ -31,47 +31,77 @@
 // document.addEventListener('DOMContentLoaded', restore_options);
 // document.getElementById('save').addEventListener('click',
 //     save_options);
+var whitelist
+var blacklist
+
+function addToWL(key){
+    let request = $("<tr id='tr_"+ key +"'></tr>");
+    let request_url = $("<td class='keyName'>").text(key);;
+    let btn_delete = $("<td><button>Delete</button></td>")
+    btn_delete.click(function () {
+        var request = $(this).parent()
+        request.hide()
+        var key = request.find(".keyName").text();
+        delete whitelist[key];
+        // save the new whitelist into local storage
+        chrome.storage.sync.set({ "whitelist": whitelist });
+    })
+    request.append(request_url);
+    request.append(btn_delete);
+    $("#tbody_wl").append(request);
+}
+
+function addToBL(key){
+    let request = $("<tr id='tr_"+ key +"'></tr>");
+    let request_url = $("<td class='keyName'>").text(key);;
+    let btn_delete = $("<td><button>Delete</button></td>")
+    btn_delete.click(function () {
+        var request = $(this).parent()
+        request.hide()
+        var key = request.find(".keyName").text();
+        delete blacklist[key];
+        console.log(blacklist)
+        console.log(key)
+        chrome.storage.sync.set({ "blacklist": blacklist});
+    })
+    request.append(request_url);
+    request.append(btn_delete);
+    $("#tbody_bl").append(request);
+}
+$("#form_addToWL").submit((e)=>{
+    e.preventDefault();
+    let url = $("#input_wlURL").val();
+    if(blacklist[url] != undefined || blacklist[url]!=null){
+        alert("This url is found in blacklist! Please click the \"move to WL\" button next to the URL");
+    } else {
+        whitelist[url] = true;
+        chrome.storage.sync.set({ "whitelist": whitelist });
+        addToWL(url);
+    }
+})
+
+$("#form_addToBL").submit((e)=>{
+    e.preventDefault();
+    let url = $("#input_blURL").val();
+    if(whitelist[url] != undefined || whitelist[url]!=null){
+        alert("This url is found in whitelist! Please click the \"move to BL\" button next to the URL");
+    } else {
+        blacklist[url] = true;
+        chrome.storage.sync.set({ "blacklist": blacklist});
+        addToBL(url);
+    }
+})
 
 chrome.storage.sync.get(["whitelist", "blacklist"], (result) => {
-    var whitelist = result.whitelist;
-    var blacklist = result.blacklist;
+    whitelist = result.whitelist;
+    blacklist = result.blacklist;
     console.log(whitelist)
     for (var key in whitelist) {
-        let request = $("<tr id='tr_"+ key +"'></tr>");
-        let request_url = $("<td class='keyName'>").text(key);;
-        let btn_delete = $("<td><button>Delete</button></td>")
-        btn_delete.click(function () {
-            var request = $(this).parent()
-            request.hide()
-            var key = request.find(".keyName").text();
-            delete whitelist[key];
-            console.log(whitelist)
-            console.log(key)
-            // save the new whitelist into local storage
-            chrome.storage.sync.set({ "whitelist": whitelist });
-        })
-        request.append(request_url);
-        request.append(btn_delete);
-        $("#tbody_wl").append(request);
+        addToWL(key);
     }
 
     for (var key in blacklist) {
-        let request = $("<tr id='tr_"+ key +"'></tr>");
-        let request_url = $("<td class='keyName'>").text(key);;
-        let btn_delete = $("<td><button>Delete</button></td>")
-        btn_delete.click(function () {
-            var request = $(this).parent()
-            request.hide()
-            var key = request.find(".keyName").text();
-            delete blacklist[key];
-            console.log(blacklist)
-            console.log(key)
-            // save the new whitelist into local storage
-            chrome.storage.sync.set({ "blacklist": blacklist});
-        })
-        request.append(request_url);
-        request.append(btn_delete);
-        $("#tbody_bl").append(request);
+        addToBL(key);
     }
 
 });
