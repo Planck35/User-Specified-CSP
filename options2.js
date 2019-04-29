b7049b54c3c3c90cbdfe1ff213e2c7fc7eff379c
ErguiXie <ergui.xie@sv.cmu.edu>
2019-04-29 00:22:55 -0700

    show help text for types

--- options2.js
+++ options2.js
@@ -1,16 +1,30 @@
 var blacktype;
 var types = ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"];
+var typeText = []
 
+const helpTextScript = new HelpTextScript();
 for (var i = 0; i < types.length; i++) {
     var resource = $("<tr></tr>");
     var btn = $('<td><label class="switch"><input type="checkbox"/><span class="slider round"></span></label></td>');
     var info = $('<td class="align-middle"></td>');
+    const btnName = "btn_help_"+i;
+    var questionMark = $("<td class=\"align-middle\"><button id=" + btnName + " class =\"help-icon btn-primary\">?</td>")
     btn.find("input").attr("id", types[i] + "-checkbox");
     btn.find("input").attr("resource-type", types[i]);
+
     info.text(types[i]);
     resource.append(btn);
     resource.append(info);
+    resource.append(questionMark)
     $("#resources").append(resource);
+    const typeName = types[i]
+    const textContent = helpTextScript.getHelpText(i)
+    $("#"+btnName).on("click",(e)=>{
+        // alert(typeName)
+        $("#modal-title-type-name").text(typeName)
+        $("#modal-text-help").text(textContent)
+        $("#btn_showHelpModal").click();
+    })
 }
 
 var save_btn = $('<tr><td><button id="save-type-setting" class="btn btn-outline-primary">Save</button></td><td></td></tr>');
@@ -22,7 +36,7 @@ chrome.storage.sync.get(["blacktype"], (result) => {
     if (blacktype == undefined) {
         blacktype = {};
     }
-    
+
     for (var i = 0; i < types.length; i++) {
         if (types[i] in blacktype) {
             $("#" + types[i] + "-checkbox").prop('checked', blacktype[types[i]]);
